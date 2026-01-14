5437d7f1ab03ea512eddd8d6605b6fbdd3a61beb
Gaurav kumar <gauravkrgrd@gmail.com>
2026-01-14 20:43:37 +0530

    completed my day3 work

---
+++ Day-3/assigmentday3/Q3.py
@@ -0,0 +1,28 @@
+class NumberIterator:
+    def __init__(self, n):
+        self.current = 1
+        self.n = n
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.current > self.n:
+            raise StopIteration
+        value = self.current
+        self.current += 1
+        return value
+
+def number_generator(n):
+    current = 1
+    while current <= n:
+        yield current
+        current += 1
+
+n = 5
+
+for i in NumberIterator(n):
+    print(i)
+
+for i in number_generator(n):
+    print(i)
